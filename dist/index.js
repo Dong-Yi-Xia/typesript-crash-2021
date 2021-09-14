7b9ec2525eb516d1a9f17eebe2764da93dcedc29
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-09-14 16:30:38 -0400

    interface

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,51 @@
 "use strict";
-// create variable name,  add the type after
 let id = 5;
-// id will be an error since id is a number, can't be assign as a string type
-// id = "A string"
+let food = 'hamburger';
+let isTall = true;
+let x = 'any type and can be reassingn';
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'hello'];
+let person = [1, 'Fred', true];
+let employee;
+employee = [
+    [1, 'Bob'],
+    [2, 'Kev'],
+    [3, 'Mie'],
+    [4, 'Pal'],
+];
+let code;
+code = 22;
+code = 'ten';
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: 'John'
+};
+let cid = 1;
+let customerID = cid;
+function addNum(x, y) {
+    return x + y;
+}
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'John',
+};
+const p1 = 1;
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,114 @@
+// Basic Tyoes
 // create variable name,  add the type after
 let id: number = 5
+let food: string = 'hamburger'
+let isTall: boolean = true
+let x: any = 'any type and can be reassingn'
 
-// id will be an error since id is a number, can't be assign as a string type
-// id = "A string"
+// id = "A string"   --> will be an error since id is a number, can't be assign as a string type
 
 
+// An array of only type number
+let ids: number[] = [1,2,3,4,5]
+let arr: any[] = [1, true, 'hello']
 
+// ids.push('Hello')   --> will error, string is not assignable to type number
+
+
+//Tuple
+let person: [number, string, boolean] = [1, 'Fred', true]
+// the argument order matter
+
+//Tuple Array
+let employee: [number, string][]
+
+employee = [
+  [1, 'Bob'],
+  [2, 'Kev'],
+  [3, 'Mie'],
+  [4, 'Pal'],
+]
+
+
+// Union
+let code: string | number
+code = 22
+code = 'ten'
+
+
+// Enum - can be number or string
+enum Direction1{
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2{
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
+}
+
+
+// Objects
+type User = {
+  id:number,
+  name: string
+}
+
+const user: User = {
+  id: 1,
+  name: 'John'
+}
+
+
+// Type Assertion
+let cid: any = 1
+
+// Two ways.
+// let customerID = <number>cid  --> cid type was any , now asseting it as a number
+let customerID = cid as number
+
+
+//Functions
+//Arg type, Function return value as a number as well
+function addNum(x: number, y: number): number {
+  return x + y
+}
+
+
+// Void  - return value isn't anything
+function log(message: string | number): void {
+  console.log(message)
+}
+
+
+// Interfaces  ---> Use with objects
+interface UserInterface {
+  readonly id:number
+  name: string
+  age?: number //optional argument
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'John',
+}
+
+// --> types can be used primitive and unions
+type Point = number | string
+const p1: Point = 1
+
+// user1.id = 5 will error because readonly
+
+// Interface with Function
+interface MathFunc {
+  (x:number, y:number): number
+}
+
+const add: MathFunc = (x:number, y:number): number => x + y
+const sub: MathFunc = (x:number, y:number): number => x - y
+
+// Classes
